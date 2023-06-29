1670a4e9ec3f34785aadda8496ae2adcf76583ec
Landeo4 <theo91.pot@gmail.com>
2023-06-29 13:35:57 +0200

    olala

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tpotilli <tpotilli@student.42.fr>          +#+  +:+       +#+        */
+/*   By: landeo <landeo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 14:14:19 by tpotilli@st       #+#    #+#             */
-/*   Updated: 2023/06/27 08:23:25 by tpotilli         ###   ########.fr       */
+/*   Updated: 2023/06/29 13:34:31 by landeo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
 		i--;
 	}
 	ft_algo_manager(&data, argc - 1);
-	//ft_print_lista(&data);
+	ft_print_lista(&data);
 	ft_free_lista(&data);
 	ft_free_listb(&data);
 }
